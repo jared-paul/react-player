86a0fb7ec839f74c406048af085c82c0f78785fa
jared-paul <jaredpaul9820@gmail.com>
2023-10-13 17:37:30 -0700

    Revert "prepublish and add spotify"
    This reverts commit c0815db2a9eab075b2f44df4bf7cec563286257f.

--- spotify.js
+++
@@ -1,9 +0,0 @@
-
-      var createReactPlayer = require('./lib/ReactPlayer').createReactPlayer
-      var Player = require('./lib/players/Spotify').default
-      module.exports = createReactPlayer([{
-        key: 'spotify',
-        canPlay: Player.canPlay,
-        lazyPlayer: Player
-      }])
-    
